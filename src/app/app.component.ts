38f46deb113fca95a8dc8e7ca1205214103a9f88
Shanoi <olivboulet@laposte.net>
2017-06-15 16:32:32 +0200

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,29 @@ import {Component} from "@angular/core";
 import {Observable} from "rxjs/Observable";
 import {ChannelService} from "../shared/services/channel/channel.service";
 import {ChanelModel} from "../shared/models/ChannelModel";
+import {NameService} from "../shared/services/name/name.service";
+import {isUndefined} from "util";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
 
-  public title: string;
-  public name: string;
-  private dispChannels: boolean;
+    public title: string;
+    public name: string;
+    private dispChannels: boolean;
 
-  constructor(private channelService: ChannelService) {
-    this.title = "Chat";
-    Observable.create();
-  }
+    private pseudoSet: boolean;
 
-  displayChannels() {
-    this.dispChannels = !this.dispChannels;
-  }
+    constructor(private channelService: ChannelService, private nameService: NameService) {
+        this.title = "Chat";
+        Observable.create();
+    }
+
+    displayChannels() {
+        this.dispChannels = !this.dispChannels;
+    }
+
 }
---
+++ src/shared/services/bot/bot.service.ts
@@ -0,0 +1,62 @@
+import {Injectable} from "@angular/core";
+import {Headers, Http, RequestOptions, Response} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import {MessageModel} from "../../models/MessageModel";
+import {ReplaySubject} from "rxjs/ReplaySubject";
+import {URLSERVER} from "shared/constants/urls";
+import {Body} from "@angular/http/src/body";
+
+@Injectable()
+export class BotService {
+
+    /**
+     * Url pour accéder aux données. L'url est commun à toutes les fonctions du service.
+     * Il permet d'accéder aux channel. À partir de cet url, vous pourrez accéder aux messages.
+     * La documentation des methodes du service permet d'avoir plus d'information concernant la façon d'accèder aux messages.
+     */
+    private url: string;
+
+    /**
+     * MessageList$ est un type d'Observable particulier appelé ReplaySubject.
+     * MessageList$ est un flux d'évenements qui stock la liste des messages. A chaque fois que l'on fait une requète
+     * pour récupérer la liste des messages, messageList$ va pousser cette nouvelle liste dans son flux pour permettre
+     * aux composants qui l'écoutent de récupérer les messages. Pour plus d'infos sur les observables, voir le README.md du projet
+     * dans lequel vous trouverez une première explication sur les observables ainsi qu'une vidéo tutoriel.
+     */
+    public messageList$: ReplaySubject<MessageModel[]>;
+
+    constructor(private http: Http) {
+        this.url = URLSERVER;
+        this.messageList$ = new ReplaySubject(1);
+    }
+
+    public getMessages(route: string) {
+        const finalUrl = this.url + route;
+        this.http.get(finalUrl)
+            .subscribe((response) => this.extractAndUpdateMessageList(response));
+    }
+
+    public sendMessage(route: string, message: MessageModel) {
+        const finalUrl = this.url + route.toString();
+        const headers = new Headers({"Content-Type": "application/json", "Authorization": "Bearer 4b8a1633fe1e4cadb3754b8fc05ae5a3 "});
+        const body = "";
+        const options = new RequestOptions({headers: headers});
+        this.http.post(finalUrl, body, options).subscribe((response) => this.extractMessageAndGetMessages(response, route));
+    }
+
+    extractAndUpdateMessageList(response: Response) {
+        // Plus d'info sur Response ou sur la fonction .json()? si tu utilises Webstorm,
+        // fait CTRL + Click pour voir la déclaration et la documentation
+        const responseMessageList = response.json().reverse() || []; // ExtractMessage: Si response.json() est undefined ou null,
+        // messageList prendra la valeur tableau vide: [];
+        this.messageList$.next(responseMessageList); // On pousse les nouvelles données dans l'attribut messageList$
+    }
+
+    private extractMessageAndGetMessages(response: Response, route: string): MessageModel {
+        this.getMessages(route);
+        return response.json();
+    }
+}
